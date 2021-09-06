07dc679134e385fa318cd1d12ed33f665e1d7f3d
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2021-09-06 22:26:57 +0700

    New Cards

--- components/database.ts
+++ components/database.ts
@@ -39,5 +39,85 @@ export const Element:ElementType[] = [
         name: 'Fai',
         picture_path: 'Fai2.png',
         rate: 'C'
+    },{
+        name: 'Mark',
+        picture_path: 'Mark.png',
+        rate: 'R'
+    },{
+        name: 'Japan',
+        picture_path: 'Japan.png',
+        rate: 'C'
+    },{
+        name: 'Princess',
+        picture_path: 'princess.png',
+        rate: 'C'
+    },{
+        name: 'Nithi',
+        picture_path: 'Nithi.png',
+        rate: 'R'
+    },{
+        name: 'Gun',
+        picture_path: 'Gun.png',
+        rate: 'C'
+    },{
+        name: 'Idin',
+        picture_path: 'Idin2.png',
+        rate: 'C'
+    },{
+        name: 'Ink',
+        picture_path: 'Ink.jpg',
+        rate: 'C'
+    },{
+        name: 'Mark',
+        picture_path: 'Mark.jpg',
+        rate: 'R'
+    },{
+        name: 'Ink',
+        picture_path: 'Ink2.jpg',
+        rate: 'SSR'
+    },{
+        name: 'Krachu',
+        picture_path: 'Krachu.JPG',
+        rate: 'R'
+    },{
+        name: 'Pha and Japan',
+        picture_path: 'PhaJapan.jpg',
+        rate: 'R'
+    },{
+        name: 'Ice and Reo',
+        picture_path: 'IceReo.jpg',
+        rate: 'R'
+    },{
+        name: 'Idin',
+        picture_path: 'Idin3.jpg',
+        rate: 'C'
+    },{
+        name: 'Japan',
+        picture_path: 'Japan2.jpg',
+        rate: 'SSR'
+    },{
+        name: 'Gun',
+        picture_path: 'Gun2.jpg',
+        rate: 'R'
+    },{
+        name: 'Q',
+        picture_path: 'Q.jpg',
+        rate: 'C'
+    },{
+        name: 'Ink',
+        picture_path: 'Ink3.jpg',
+        rate: 'R'
+    },{
+        name: 'Mark',
+        picture_path: 'Mark2.jpg',
+        rate: 'R'
+    },{
+        name: 'Ink',
+        picture_path: 'Ink4.jpg',
+        rate: 'C'
+    },{
+        name: 'Pha',
+        picture_path: 'Pha3.png',
+        rate: 'R'
     }
 ]
